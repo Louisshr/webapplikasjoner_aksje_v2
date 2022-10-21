c34b909d1c56fddb7b1f44c455c57ff45c12b1f8
LouisVitenskap <70730534+LouisVitenskap@users.noreply.github.com>
2022-10-21 22:12:40 +0200

    Lastet opp aksje programmet

---
+++ aksje2/wwwroot/js/index.js
@@ -0,0 +1,113 @@
+﻿let personId = 1;
+localStorage.setItem('data', personId);
+
+$(function () {
+    hentAlleAksjer();
+})
+
+function hentAlleAksjer() {
+    $.get("aksje/hentAksjer", function (aksjer) {
+        if (aksjer != null) {
+            formaterAksjer(aksjer);
+        }
+        else {
+            // midlertidig
+
+            console.log("Ikke mulig å hente aksjer");
+        }
+    });
+
+}
+
+
+
+function formaterAksjer(aksjer) {
+    let ut = "<table class='table table-sm'>" + "<thead class='thead-light'>" + "<tr>" +
+        "<th scope='col'>Navn</th>" +
+        "<th style='width: 200px' scope='col'>Lav</th>" +
+        "<th style='width: 200px' scope='col'>Høy</th>" +
+        "<th style='width: 200px' scope='col'>Åpning</th>" +
+        "<th style='width: 200px' scope='col'>Kjøp</th>" +
+        "<th style='width: 200px' scope='col'>Omsetning</th>" +
+        "<th style='width: 200px' scope='col'></th>" +
+        "</thead>" +
+        "<tbody>";
+
+
+    for (let aksje of aksjer) {
+        ut += "<tr>" +
+            "<th scope='row'>" + aksje.navn + "</th>" +
+            "<td>" + aksje.low + "</td>" +
+            "<td>" + aksje.high + "</td>" +
+            "<td>" + aksje.open + "</td>" +
+            "<td>" + aksje.verdi + "</td>" +
+            "<td>" + aksje.omsetning + "</td>" +
+            "<td> <button type='button' class='btn btn-success' data-toggle='modal' data-target='#kjop_popupBox' onclick='kjop(" + aksje.id + ")'>Kjøp</button></td>"
+        "</tr>";
+    }
+
+    ut += "</table>";
+
+    document.querySelector(".aksje-oversikt").innerHTML = ut;
+}
+
+function kjop(id) {
+    const url = "aksje/hentEn?id=" + id;
+
+    $.get(url, function (aksje) {
+        if (aksje !== null) {
+            //informasjon skal kun opprettes hvis get kallet er suksessfullt (skal være en if setning)
+            //kan hende at bestilling navn og kurs bør defineres utenfor en funksjon slik at man ikke trenger å hente de hver gang
+
+            document.getElementById("popupBox").innerHTML = "Kjøp " + "(" + aksje.navn + ")";
+            document.getElementById("kjop-knapp").onclick = function () { fullforKjop(aksje.id, aksje.verdi, aksje.navn); }
+        }
+        else {
+            console.log("Kjop: Ikke mulig å hente aksje")
+        }
+
+    });
+
+}
+
+
+function fullforKjop(id, verdi, aksje) {
+
+    let antall_input = document.getElementById("typeNumber").value;
+    let antall = Number(antall_input);
+    let melding_ut = document.getElementById("melding_ut");
+
+    // mulig å endre slik at det vises forskjellig feilmelding avhengig av om input er bokstaver eller om input feltet er tomt.
+
+    if (isNaN(antall) || antall_input == '') {
+        document.getElementById("bestilling-feil").innerHTML = "Venligst oppgi et gyldig antall";
+    }
+    else {
+        let total_pris = verdi * antall;
+
+        let salgObjekt =
+        {
+            person: personId,
+            antall: antall,
+            pris: total_pris,
+            aksje: id
+        }
+        $.post("aksje/kjopAksje", salgObjekt, function (OK) {
+            let ut = "";
+
+            if (OK) {
+                ut = "Kjøp er gjennomført. Du handlet " + antall + " " + aksje + " aksjer. ";
+            }
+            else {
+                ut = "Det oppstod en feil under kjøpet. Kjøpet er avbrutt";
+            }
+
+            console.log(ut);
+
+
+        })
+
+
+    }
+
+}
---
+++ aksje2/wwwroot/js/portefolje.js
@@ -0,0 +1,102 @@
+﻿let id = localStorage.getItem('data');
+
+$(function () {
+    hentSaldo();
+    hentPortefolje();
+})
+
+
+function hentSaldo() {
+    const url = "aksje/hentSaldo?id=" + id;
+
+    $.get(url, function (saldo) {
+        if (saldo != -1) {
+            document.getElementById("felt-for-saldo").innerHTML = saldo.toFixed(2) + " USD";
+        }
+        else {
+            console.log("Det har oppstått en feil, saldo er ikke tilgjengelig");
+        }
+
+    })
+}
+
+function hentPortefolje() {
+    const url = "aksje/hentPortefolje?id=" + id;
+
+    $.get(url, function (portefolje) {
+
+        if (portefolje != null) {
+            formaterPortefolje(portefolje);
+        }
+        else {
+            console.log("Det har oppstått en feil. Venligst prøv igjen senere");
+        }
+    })
+}
+
+function formaterPortefolje(portefolje) {
+    let ut = "<table class='table'><thead class='thead-light'>" + "<tr>" +
+        "<th scope='col'>Navn</th>" +
+        "<th scope='col'>Antall</th>" +
+        "<th scope='col'>Verdi (USD)</th>" +
+        "<th style='width: 150px' scope = 'col'>selg</th>" +
+        "</tr></thead><tbody>";
+
+    for (let aksje of portefolje) {
+        ut += "<tr>" +
+            "<th scope='row'>" + aksje.aksje.navn + "</th>" +
+            "<td>" + aksje.antall + "</td>" +
+            "<td>" + aksje.pris.toFixed(2) + "</td>" +
+            "<td> <button class='btn btn-danger' data-toggle='modal' data-target='#selg_popupBox' onclick='selg(" + aksje.aksje.id + ")'>selg</button></td>" +
+            "</tr>";
+
+        console.log(aksje.aksje.id);
+    }
+
+    ut += "</tbody></table>";
+    document.querySelector(".portefolje-container").innerHTML = ut;
+}
+
+function selg(id) {
+    const url = "aksje/hentEn?id=" + id;
+
+    $.get(url, function (aksje) {
+        //informasjon skal kun opprettes hvis get kallet er suksessfullt
+        //kan hende at bestilling navn og kurs bør defineres utenfor en funksjon slik at man ikke trenger å hente de hver gang
+
+        document.getElementById("popupBox").innerHTML = "Selg " + "(" + aksje.navn + ")";
+        document.getElementById("selg-knapp").onclick = function () { fullforSalg(aksje.id, aksje.verdi, aksje.navn); }
+    });
+
+}
+
+
+function fullforSalg(aksje_id, verdi, aksje_navn) {
+    let input_antall = document.getElementById("typeNumber");
+    let antall = Number(input_antall.value);
+
+    if (isNaN(antall) || input_antall == '') {
+        console.log("Venligst oppgi et gyldig antall");
+    }
+
+    let selg_objekt =
+    {
+        personId: id,
+        aksjeId: aksje_id,
+        antall: antall
+    }
+
+    $.post("aksje/selg", selg_objekt, function (OK) {
+        if (OK) {
+            console.log("Solgt: " + aksje_navn + ", verdi: " + verdi * antall);
+            hentSaldo();
+            hentPortefolje();
+            console.log(OK);
+        }
+        else {
+            // midlertidig
+
+            console.log("Selg: Det har oppstått en feil")
+        }
+    })
+}
